48c1dcc7f233db5ce98e10b2d6db9e8679239604
lubcountcooper <lub@countcooper.nl>
2019-02-22 13:59:15 +0800

    pushing the work so far week 1

---
+++ src/06_classes_objects_methods/Challenge_game.py
@@ -0,0 +1,20 @@
+
+from Challenge_classes import *
+
+stack1 = Deck()
+stack1.shuffle()
+
+handsize = 5
+players = [player1, player2, player3]
+playerssize = players.__len__()
+
+for i in players:
+    players[0]
+
+
+for i  in range(handsize):
+    for j in range(playerssize):
+        players(j)
+
+
+player1 = Hand()
